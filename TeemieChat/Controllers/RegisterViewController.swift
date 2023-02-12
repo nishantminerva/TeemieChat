a4d50f288585f7a2c7e1d7b5f75cb1f8c52120e6
Nishant Minerva <nishantcafe@gmail.com>
2023-02-13 00:19:14 +0530

    Adding Firebase to Incorporate Registration and Login for Users

--- TeemieChat/Controllers/RegisterViewController.swift
+++ TeemieChat/Controllers/RegisterViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -13,6 +16,17 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        let email = emailTextfield.text!
+        let password = passwordTextfield.text!
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                                print(e)
+                            } else {
+                                //Navigate to the ChatViewController
+                                self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                            }
+        }
     }
     
 }
